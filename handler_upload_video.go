d44dfb438e6e05338fbbc0872e70bd7ec9068384
Crawcraw <christophe.raucroy@gmail.com>
2025-07-06 19:17:14 +0200

    prefix video by landcape, portrait or other. process for fast start

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
+	"math"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 	"slices"
 	"strings"
 
@@ -17,9 +21,73 @@ import (
 	"github.com/google/uuid"
 )
 
+func getRatio(w int, h int) (float64, error) {
+	if (h == 0) {
+		return math.NaN(), fmt.Errorf("cannot divide by zero")
+	}
+	return float64(w) / float64(h), nil
+}
+
+func floatsEqual(f1 float64, f2 float64) bool {
+	return math.Abs(f1 - f2) <= 0.01
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	var buf bytes.Buffer;
+	cmd.Stdout = &buf;
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("error while getting video format: %w", err)
+	}
+
+	type streamData struct {
+		Width int  `json:"width"`
+		Height int `json:"height"`
+	}
+
+	type videoDataInfo struct {
+		Streams []streamData `json:"streams"`
+	}
+	var videoData videoDataInfo
+	err = json.Unmarshal(buf.Bytes(), &videoData)
+	if err != nil {
+		return "", fmt.Errorf("error while parsing JSON from video format command: %w", err)
+	}
+
+	ratio, err := getRatio(videoData.Streams[0].Width, videoData.Streams[0].Height)
+	if err != nil {
+		return "", fmt.Errorf("error while computing ratio: %w", err)
+	}
+
+	landRatio, _ := getRatio(16, 9)
+	portRatio, _ := getRatio(9, 16)
+
+	var ratioStr string
+	if floatsEqual(ratio, landRatio) {
+		ratioStr = "landscape"
+	} else if floatsEqual(ratio, portRatio) {
+		ratioStr = "portrait"
+	} else {
+		ratioStr = "other"
+	}
+	return ratioStr, nil
+}
+
+func processVideoForFastStart(filePath string) (string, error) {
+	output := filePath + ".processing"
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", output)
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("error while processing video for faststart: %w", err)
+	}
+	return output, nil
+}
+
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 	http.MaxBytesReader(w, r.Body, 1 << 30)
 
+	// Retrieve videoID parameter
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -27,6 +95,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// Authentication
 	token, err := auth.GetBearerToken(r.Header)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
@@ -39,22 +108,26 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// Retrieve video from DB
 	metadata, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusNotFound, "Couldn't find video", err)
 		return
 	}
+	// Check logged in user is the owner
 	if metadata.UserID != userID {
 		respondWithError(w, http.StatusNotFound, "Unauthorized", fmt.Errorf("Unauthorized"))
 		return
 	}
 
+	// Retrieve video data from request
 	data, head, err := r.FormFile("video")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Ill-formed request", err)
 	}
 	defer data.Close()
 
+	// Check content type
 	cType, _, _ := mime.ParseMediaType(head.Header.Get("Content-Type"))
 	supportedTypes := []string{"video/mp4"}
 	if !slices.Contains(supportedTypes, cType) {
@@ -62,6 +135,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// Create temporary file and writing data into it
 	temp, err := os.CreateTemp("", "tubely-upload.mp4")
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "error while creating temp file", fmt.Errorf("error while creating temp file"))
@@ -75,26 +149,47 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	_, err = temp.Seek(0, io.SeekStart)
+	// Get aspect ratio
+	aspect, err := getVideoAspectRatio(temp.Name())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "error while resetting temp file seek position", fmt.Errorf("error while resetting temp file seek position"))
+		respondWithError(w, http.StatusInternalServerError, "error while trying to determine aspect ration of uploaded video", fmt.Errorf("error while trying to determine aspect ration of uploaded video"))
 		return
 	}
 
+	// Process video file
+	output, err := processVideoForFastStart(temp.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error while processing video file for fast start", fmt.Errorf("error while processing video file for fast start"))
+		return
+	}
+
+	outputFile, err := os.Open(output)
+	defer os.Remove(outputFile.Name())
+	defer outputFile.Close()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error while opening processed file", fmt.Errorf("error while opening processed file"))
+		return
+	}
+
+	// Generate 32-byte length random string
 	rnd := make([]byte, 32)
 	rand.Read(rnd)
 	buf := strings.Builder{}
 	encoder := base64.NewEncoder(base64.RawURLEncoding, &buf)
 	encoder.Write(rnd)
 	encoder.Close()
-	key := fmt.Sprintf("%s.mp4", buf.String())
+	
+	// Craft S3 key with aspect and previous random string
+	key := fmt.Sprintf("%v/%s.mp4", aspect, buf.String())
 
-	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{Bucket: &cfg.s3Bucket, Key: &key, Body: temp, ContentType: aws.String("video/mp4")})
+	// Upload to S3 bucket
+	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{Bucket: &cfg.s3Bucket, Key: &key, Body: outputFile, ContentType: aws.String("video/mp4")})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "error while upload file to S3 bucket", fmt.Errorf("error while upload file to S3 bucket"))
 		return
 	}
 
+	// Update video metadata with AWS S3 URL
 	url := fmt.Sprintf("https://%v.s3.%v.amazonaws.com/%v", cfg.s3Bucket, cfg.s3Region, key)
 	metadata.VideoURL = &url
 	err = cfg.db.UpdateVideo(metadata)
@@ -103,5 +198,6 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// Respond with OK status
 	respondWithJSON(w, http.StatusOK, metadata)
 }
